5d9c7ab8834f8ccacaf662b673c5a0df738892f1
g2213swo <2057581537@qq.com>
2023-02-21 22:30:19 +0800

    I am sb

--- src/main/java/me/xiaozhangup/monkeygrapplinghook/MonkeyGrapplingHook.java
+++ src/main/java/me/xiaozhangup/monkeygrapplinghook/MonkeyGrapplingHook.java
@@ -13,7 +13,11 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerFishEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerItemHeldEvent;
+import org.bukkit.event.player.PlayerSwapHandItemsEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.Damageable;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
@@ -21,13 +25,15 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class MonkeyGrapplingHook extends JavaPlugin implements Listener {
 
     public static Plugin plugin;
     public static ListenerManager listenerManager = new ListenerManager();
-    public NamespacedKey namespace;
+    public static NamespacedKey namespace;
 
     @Override
     public void onEnable() {
@@ -40,7 +46,7 @@ public class MonkeyGrapplingHook extends JavaPlugin implements Listener {
         listenerManager.register();
 
         Command.register("gethook", (commandSender, command, s, inside) -> {
-            if (commandSender.isOp() && commandSender instanceof Player p) {
+            if (!inside[0].equalsIgnoreCase("") && commandSender.isOp() && commandSender instanceof Player p) {
                 p.getInventory().addItem(getHook(Integer.parseInt(inside[0])));
             }
             return false;
@@ -54,28 +60,39 @@ public class MonkeyGrapplingHook extends JavaPlugin implements Listener {
         ItemMeta itemMeta = itemStack.getItemMeta();
         PersistentDataContainer data = itemMeta.getPersistentDataContainer();
         data.set(namespace, PersistentDataType.STRING, "true");
-        itemStack.setItemMeta(itemMeta);
-        itemStack.setDurability((short) dur);
-
+        if (itemMeta instanceof Damageable meta){
+            meta.setDamage(dur);
+            itemStack.setItemMeta(itemMeta);
+        }
         return itemStack;
     }
 
     @EventHandler
     public void on(PlayerFishEvent e) {
         if (e.getState() == PlayerFishEvent.State.REEL_IN || e.getState() == PlayerFishEvent.State.IN_GROUND) {
+            ItemStack itemInMainHand = e.getPlayer().getInventory().getItemInMainHand();
+            ItemStack itemInOffHand = e.getPlayer().getInventory().getItemInOffHand();
+            hookerMethod(e, itemInMainHand);
+            hookerMethod(e, itemInOffHand);
+        }
+    }
+    private static void hookerMethod(PlayerFishEvent e, ItemStack item){
             Player player = e.getPlayer();
-            ItemStack item = player.getInventory().getItemInMainHand();
+
             ItemMeta meta = item.getItemMeta();
+            Damageable itemDamage = (Damageable) meta;
             try {
                 PersistentDataContainer data = meta.getPersistentDataContainer();
                 String ret = data.get(namespace, PersistentDataType.STRING);
                 if (Objects.equals(ret, "true")) {
                     Hooker.normalPush(player, e.getHook(), 1.2);
-                    if (item.getType().getMaxDurability() < item.getDurability()) {
+                    if (item.getType().getMaxDurability() < itemDamage.getDamage()) {
                         item.setType(Material.AIR);
                         sendTitle(player, "抓钩已损坏", 210, 15, 57);
                     } else {
-                        item.setDurability((short) (item.getDurability() + 1));
+                        //item.setDurability((short) (item.getDurability() + 1));
+                        itemDamage.setDamage(itemDamage.getDamage() + 1);
+                        item.setItemMeta(itemDamage);
                         if (item.getType().getMaxDurability() == item.getDurability()) {
                             sendTitle(player, "抓钩已损坏", 210, 15, 57);
                         } else if ((item.getType().getMaxDurability() - item.getDurability()) < 6) {
@@ -83,11 +100,8 @@ public class MonkeyGrapplingHook extends JavaPlugin implements Listener {
                         }
                     }
                 }
-            } catch (Exception ignored) {
-            }
-        }
+            } catch (Exception ignored) {}
     }
-
     private static void sendTitle(Player player, String 抓钩即将损坏, int r, int g, int b) {
         player.resetTitle();
         player.sendTitlePart(TitlePart.TIMES, Title.Times.times(Duration.ofSeconds(0),Duration.ofSeconds(1),Duration.ofSeconds(0)));
